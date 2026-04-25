c3800bb2d125377da9546e3206715a897680d31e
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-04-25 17:21:06 -0300

    feat(app): organize middleware and routes setup in app.ts

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,16 +1,30 @@
+// apps/api/src/app.ts
+
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import dotenv from 'dotenv'
+
+import { errorHandler } from './shared/errors/error-handler.js'
+import { notFoundMiddleware } from './shared/middlewares/not-found.middleware.js'
+
+import { authRoutes } from './modules/auth/auth.routes.js'
+// import { linksRoutes } from './modules/links/links.routes.js'
+// import { redirectRoutes } from './modules/redirects/redirects.routes.js'
+// import { analyticsRoutes } from './modules/analytics/analytics.routes.js'
 
 dotenv.config()
 
 export const app = express()
 
 app.use(helmet())
-app.use(cors({
-  origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
-}))
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
+  }),
+)
+
 app.use(express.json())
 
 app.get('/health', (_req, res) => {
@@ -19,3 +33,12 @@ app.get('/health', (_req, res) => {
     app: 'LinkPulse API',
   })
 })
+
+app.use('/api/v1/auth', authRoutes)
+
+// app.use('/api/v1/links', linksRoutes)
+// app.use('/r', redirectRoutes)
+// app.use('/api/v1/analytics', analyticsRoutes)
+
+app.use(notFoundMiddleware)
+app.use(errorHandler)
